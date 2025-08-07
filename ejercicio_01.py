57e7142d8a47c2edcb4b8aa9dc8923510e667374
Maykmora <maykmora12@gmail.com>
2025-08-07 09:47:07 -0600

    commit 1 estructura del programa

---
+++ ejercicio_01.py
@@ -0,0 +1,19 @@
+def menu():
+    print("1.Agregar libros")
+    print("2.Mostrar lista de libros")
+    print("3.Eliminar libros por titulo")
+    print("4.Salir")
+
+
+while True:
+    menu()
+    option=int(input("\nSeleccione una opción del menú (1-4"))
+    match option:
+        case "1":
+            print()
+        case "2":
+            print()
+        case "3":
+            print()
+        case "4":
+            print()
