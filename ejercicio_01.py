666779d81c565c9bcd5cdd5288c5c7df0aa6b25b
Maykmora <maykmora12@gmail.com>
2025-08-07 10:18:00 -0600

    commit 4 Def agregar libro

--- ejercicio_01.py
+++ ejercicio_01.py
@@ -4,9 +4,38 @@ class Libros:
         self.autor = autor
         self.anio = anio
 
+    def mostrar_info(self):
+        print(f"Titulo: {self.titulo} -Autor:{self.autor} -Año:{self.anio}")
+
 lista_libros=[]
+def agregar_libro():
+    titulo=input("Ingrese el titulo del libro: ")
+    autor=input("Ingrese el autor del libro: ")
+    try:
+        anio=int(input("Ingrese el año del libro"))
+        if anio<0:
+            print("Error, el año no puede ser menor a 0")
+        else:
+            libro=Libros(titulo, autor, anio)
+            lista_libros.append(libro)
+            print("El libro se agrego correctamente.")
+    except ValueError:
+        print("Error al ingresar el año del libro, inténtelo de nuevo")
+    except Exception as e:
+        print("Ocurrio un error inesperado, intente de nuevo")
+
 
 def mostrar_libros():
+    if not lista_libros:
+        print("No existen libros registrados.")
+    else:
+        print("\n--Lista de Libros--")
+        contador=1
+        for libro in lista_libros:
+            print(f"Libro No.{contador}")
+            libro.mostrar_info()
+            contador+=1
+
 
 
 def menu():
